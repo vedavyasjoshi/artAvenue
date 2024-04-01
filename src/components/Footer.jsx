2f77b99e70b31c1855db7fac04f79e9194e6f3f1
Vedavyas <vedavyasjoshi121@gmail.com>
2024-04-01 13:50:24 +0530

    Implements footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,92 @@
-import React from 'react'
-
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="bottom-0 w-[100vw] text-[#DDD]">
+      <div className="bg-[rgb(35,47,62)] text-sm flex justify-evenly p-5 px-80">
+        <div>
+          <h1 className="my-2 text-white text-lg font-bold">Get to Know Us</h1>
+          <h6>Careers</h6>
+          <h6>Blog</h6>
+          <h6>About Art Avenue</h6>
+          <h6>Investor Relations</h6>
+        </div>
+        <div>
+          <h1 className="my-2 text-white text-lg font-bold">
+            Make Money with Us
+          </h1>
+          <h6>Sell products on Art Avenue</h6>
+          <h6>Sell on Art Avenue Business</h6>
+          <h6>Become an Affiliate</h6>
+          <h6>Advertise Your Products</h6>
+          <h6>Self-Publish with Us</h6>
+        </div>
+        <div>
+          <h1 className="my-2 text-white text-lg font-bold">
+            Payment Products
+          </h1>
+          <h6>Art Avenue Business Card</h6>
+          <h6>Shop with Points</h6>
+          <h6>Reload Your Balance</h6>
+          <h6>Art Avenue Currency Converter</h6>
+        </div>
+        <div>
+          <h1 className="my-2 text-white text-lg font-bold">Let Us Help You</h1>
+          <h6>Art Avenue and COVID-19</h6>
+          <h6>Your Account</h6>
+          <h6>Your Orders</h6>
+          <h6>Shipping Rates & Policies</h6>
+          <h6>Returns & Replacements</h6>
+          <h6>Help</h6>
+        </div>
+      </div>
+      <div className="bg-[#131A22] pb-10 px-96">
+        <div className="text-[#DDD] text-xs flex justify-evenly p-5">
+          <div className="w-32">
+            <h1 className="my-2 text-white text-md font-bold">
+              Art Avenue Global
+            </h1>
+            <h6>Ship Orders Internationally</h6>
+          </div>
+          <div className="w-32">
+            <h1 className="my-2 text-white text-md font-bold">
+              Art Avenue Ads
+            </h1>
+            <h6>Reach customers wherever they spend their time</h6>
+          </div>
+          <div className="w-32">
+            <h1 className="my-2 text-white text-md font-bold">6pm</h1>
+            <h6>Score deals on art brands</h6>
+          </div>
+          <div className="w-32">
+            <h1 className="my-2 text-white text-md font-bold">Woot!</h1>
+            <h6>Deals and Shenanigans</h6>
+          </div>
+          <div className="w-32">
+            <h1 className="my-2 text-white text-md font-bold">
+              Art Avenue Subscription Boxes
+            </h1>
+            <h6>Top subscription boxes – right to your door</h6>
+          </div>
+          <div className="w-32">
+            <h1 className="my-2 text-white text-md font-bold">
+              Sell on Art Avenue
+            </h1>
+            <h6>Start a Selling Account</h6>
+          </div>
+        </div>
+        <div className="text-xs px-40">
+          <div className="flex justify-evenly">
+            <h6>Conditions of Use</h6>
+            <h6>Privacy Notice</h6>
+            <h6>Consumer Health Data Privacy Disclosure</h6>
+            <h6>Your Ads Privacy Choices</h6>
+          </div>
+          <h6 className="text-center">
+            © 1996-2024, Art Avenue.com, Inc. or its affiliates
+          </h6>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
